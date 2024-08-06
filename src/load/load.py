000f53be611d89822a11bddf1a49718fb613d71a
Nicou11 <lyoungin1204@gmail.com>
2024-08-06 16:50:41 +0900

    switch

--- src/load/load.py
+++ src/load/load.py
@@ -4,4 +4,4 @@ from tabulate import tabulate
 def load(path="~/tmp/team_jkl/"):
     df=pd.read_parquet("~/tmp/team_jkl/")
 
-    print(tabulate(df,headers=["movieNm","openDt","salesAmt"], tablefmt="fancy_grid"))
+    print(tabulate(df,headers=["movieNm","salesAmt","openDt"], tablefmt="fancy_grid"))
